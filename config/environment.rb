11dc4801e227c386bad86667cad49912ef74ca28
jacksonohallo <jacksonohallo@gmail.com>
2022-08-27 09:46:25 +0300

    commit tree 8ca851b2022ba20be550f3c26e534b20457662e8

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,4 @@
-# Set up Bundler here following the instructions in the README
+
+# Set up Bundler here following the instructions in the READMErequire 'bundler/setup'
+require 'bundler/setup'
+Bundler.require(:default, :development)
